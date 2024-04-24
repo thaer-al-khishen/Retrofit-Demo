19e30c15179f36c32252e797fdd091e3a8df51ec
Thaer Al Khishen <thaer.elkhishen@foo.mobi>
2024-04-24 22:05:24 +0300

    Initial setup for retrofit with http get

---
+++ app/src/main/java/com/relatablecode/retrofitdemoapplication/ApiService.kt
@@ -0,0 +1,10 @@
+package com.relatablecode.retrofitdemoapplication
+
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("/todos")
+    suspend fun getTodos(): List<TodosItem>
+
+}
--- app/src/main/java/com/relatablecode/retrofitdemoapplication/MainActivity.kt
+++ app/src/main/java/com/relatablecode/retrofitdemoapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.relatablecode.retrofitdemoapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = RetrofitClient.apiService.getTodos()
+            Log.d("Response", response.toString())
+        }
+
     }
 }
